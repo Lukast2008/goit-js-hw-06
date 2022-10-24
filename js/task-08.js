7cee619c4232120d5ff88dc1193dd25864ff7d78
Lukast2008 <lukast2008@gmail.com>
2022-10-24 18:19:34 +0300

    9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector("form");
 
+formRef.addEventListener("submit", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  const valueMail = event.target.email.value.trim();
+  const valuePassword = event.target.password.value.trim();
+
+  if (valueMail === "" || valuePassword === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    const personData = { email: valueMail, password: valuePassword };
+
+    console.log(personData);
+  }
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btnClick = document.querySelector(".change-color");
+const colorVal = document.querySelector(".color");
+
+btnClick.addEventListener("click", onClick);
+
+function onClick() {
+  const HexColor = getRandomHexColor();
+  colorVal.textContent = HexColor;
+  colorVal.style.backgroundColor = HexColor;
 }
