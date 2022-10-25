6134ed03709880a5bd82c174291dd42c1d85039e
Lukast2008 <lukast2008@gmail.com>
2022-10-25 18:14:40 +0300

    extra task done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createDivEl = document.querySelector("#boxes");
+const inputValue = document.querySelector("input");
+const buttonCreateEl = document.querySelector("[data-create]");
+const buttonRemoveEl = document.querySelector("[data-destroy]");
+
+let arr = [];
+
+function removeDivEl() {
+  createDivEl.innerHTML = "";
+  inputValue.value = "";
+  arr = [];
+}
+
+function createBoxes(amount) {
+  createDivEl.innerHTML = "";
+  arr = [];
+  for (let i = 0; i < inputValue.value; i += 1) {
+    const divItem = document.createElement("div");
+
+    divItem.style.width = `${30 + i * 10}px`;
+    divItem.style.height = `${30 + i * 10}px`;
+    divItem.style.backgroundColor = getRandomHexColor();
+    arr.push(divItem);
+  }
+  createDivEl.append(...arr);
+}
+
+buttonCreateEl.addEventListener("click", createBoxes);
+
+buttonRemoveEl.addEventListener("click", removeDivEl);
